6bcdda4ac9660b1357c97773e69017c0386e82d6
Brandon Teich <brandonteich123@gmail.com>
2019-10-05 21:22:03 +0000

    Automatically backed up by Learn

--- db/migrate/20190811005844_create_join_table_taxis_passengers.rb
+++ db/migrate/20190811005844_create_join_table_taxis_passengers.rb
@@ -1,7 +1,11 @@
-class CreateTaxis < ActiveRecord::Migration
+class CreateJoinTableTaxisPassengers < ActiveRecord::Migration[5.0]
   def change
-    create_table :taxis do |t|
-      t.timestamps null: false
+    create_join_table :taxis, :passengers do |t|
+      # t.index [:taxi_id, :passenger_id]
+      # t.index [:passenger_id, :taxi_id]
+
+       add_column :rides, :passenger_id, :integer
+    add_column :rides, :taxi_id, :integer
     end
   end
 end
